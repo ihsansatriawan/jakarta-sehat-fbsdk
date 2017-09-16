a039273cb36fa7185a9ba5cad54cfeac9ffefdc2
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2017-09-17 06:30:21 +0700

    Implement FB login

--- App/Containers/MainScreen.js
+++ App/Containers/MainScreen.js
@@ -3,10 +3,19 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Button } from 'native-base';
 import { Actions as NavigationActions } from 'react-native-router-flux';
+import FBSDK from 'react-native-fbsdk'
+
+const { LoginButton, AccessToken } = FBSDK
 
 // create a component
 class MainScreen extends React.Component {
+
+  state = {
+    isLogin: false
+  }
+
   render () {
+    const { isLogin } = this.state
     return (
       <View style={styles.container}>
         <View style={styles.boxIcon}>
@@ -18,33 +27,66 @@ class MainScreen extends React.Component {
           <View style={styles.lineBot} />
         </View>
 
-        <View style={styles.boxButton}>
-          <Button
-            onPress={() => NavigationActions.fetchlist({ paramKey: 'rsumum' })}
-            style={{
-              backgroundColor: '#028090',
-              margin: 15
-            }} full>
-            <Text style={{color: '#fff'}}>Rs Umum</Text>
-          </Button>
+        {
+          isLogin && <View style={styles.boxButton}>
+            <Button
+              onPress={() => NavigationActions.fetchlist({ paramKey: 'rsumum' })}
+              style={{
+                backgroundColor: '#028090',
+                margin: 15
+              }} full>
+              <Text style={{color: '#fff'}}>Rs Umum</Text>
+            </Button>
 
-          <Button 
-            onPress={() => NavigationActions.fetchlist({ paramKey: 'rskhusus' })}
-            style={{
-              backgroundColor: '#02C39A',
-              margin: 15
-            }} full>
-            <Text style={{color: '#fff'}}>Rs Khusus</Text>
-          </Button>
+            <Button 
+              onPress={() => NavigationActions.fetchlist({ paramKey: 'rskhusus' })}
+              style={{
+                backgroundColor: '#02C39A',
+                margin: 15
+              }} full>
+              <Text style={{color: '#fff'}}>Rs Khusus</Text>
+            </Button>
 
-          <Button 
-            onPress={() => NavigationActions.fetchlist({ paramKey: 'puskesmas' })}
-            style={{
-              backgroundColor: '#00A896',
-              margin: 15
-            }} full>
-            <Text style={{color: '#fff'}}>Puskesmas</Text>
-          </Button>
+            <Button 
+              onPress={() => NavigationActions.fetchlist({ paramKey: 'puskesmas' })}
+              style={{
+                backgroundColor: '#00A896',
+                margin: 15
+              }} full>
+              <Text style={{color: '#fff'}}>Puskesmas</Text>
+            </Button>
+            
+          </View>
+        }
+
+        <View style={{ flex:1, justifyContent: 'center', alignItems:'center' }}>       
+          <LoginButton
+            publishPermissions={["publish_actions"]}
+            onLoginFinished={
+              (error, result) => {
+                if (error) {
+                  alert("login has error: " + result.error);
+                } else if (result.isCancelled) {
+                  alert("login is cancelled.");
+                } else {
+                  AccessToken.getCurrentAccessToken().then(
+                    (data) => {
+                      this.setState({
+                        isLogin: true
+                      })
+                      console.log(data)
+                      alert(data.accessToken.toString())
+                    }
+                  )
+                }
+              }
+            }
+            onLogoutFinished={() => {
+              this.setState({
+                isLogin: false
+              })
+              alert("logout.")
+            }}/>
         </View>
       </View>
     );
--- android/app/src/main/java/com/carijakartasehat/MainApplication.java
+++ android/app/src/main/java/com/carijakartasehat/MainApplication.java
@@ -3,15 +3,26 @@ package com.carijakartasehat;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -22,7 +33,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+              new MainReactPackage(),
+              new FBSDKPackage(mCallbackManager)
       );
     }
   };
@@ -35,6 +47,8 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+    FacebookSdk.sdkInitialize(getApplicationContext());
+    // If you want to use AppEventsLogger to log events.
+    AppEventsLogger.activateApp(this);
   }
 }
